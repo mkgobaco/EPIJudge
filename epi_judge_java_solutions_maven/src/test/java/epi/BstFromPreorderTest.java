a57b0b6b04a4e1ed8aeb771e8e8d17db2c3229d8
Michael Gobaco <michael@gobaco.com>
2020-06-24 12:40:45 -0700

    Add BstFromPreOrder test

--- epi_judge_java_solutions_maven/src/test/java/epi/BstFromPreorderTest.java
+++ epi_judge_java_solutions_maven/src/test/java/epi/BstFromPreorderTest.java
@@ -1,36 +1,52 @@
 package epi;
 
 import epi.test_framework.TestUtils;
+import epi.test_framework.serialization_traits.SerializationTraits;
+import epi.test_framework.serialization_traits.TraitsFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.*;
 
-public class BstFromPreorderTest {
+@Slf4j
+public class BstFromPreorderTest extends BaseTest{
 
-    static String testDataFile = null;
-    static List<List<String>> testData = null;
+    static Class testClass = BstFromPreorder.class;
 
     @BeforeClass
-    public static void setup() throws URISyntaxException {
-        testDataFile = TestUtils.getTestDataFile(BstFromPreorder.class);
-        testData = TestUtils.splitTsvFile(testDataFile);
+    public static void beforeClass() throws URISyntaxException, NoSuchMethodException {
+        setup(testClass);
     }
 
     @Test
-    public void getTestDataFile()
-    {
-        assertEquals("bst_from_preorder.tsv", testDataFile);
-    }
-
-    @Test
-    public void rebuildBSTFromPreorder() {
+    public void rebuildBSTFromPreorder1() {
         BstNode<Integer> root =  BstFromPreorder.rebuildBSTFromPreorder(Arrays.asList(new Integer[]{1, 2, 3}));
         assertNotNull(root);
+    }
+
+    @Test
+    public void rebuildBSTFromPreorder() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method func = testClass.getDeclaredMethod("rebuildBSTFromPreorder", List.class);
+
+        for (int ii = 1; ii < testData.size(); ii++) {
+            List<String> testCase = testData.get(ii);
+            testCase = testCase.subList(0, testCase.size()-1);
+
+            BstNode result = (BstNode) TestUtils.runTest(func, testCase);
+            String expected = testCase.get(testCase.size()-1);
+            SerializationTraits retValueTraits = TraitsFactory.getTraits(func.getGenericReturnType());
+            assertEquals(retValueTraits.parse(expected), result);
+            log.debug("expected={}", retValueTraits.parse(expected));
+            log.debug("actual  ={}", result);
+
+        }
     }
 }
